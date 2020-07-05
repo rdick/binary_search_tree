ffb99d60f159bc1b573e5b0f68b7ce213114b43d
Ryan Dick <rdick@ualberta.ca>
2020-07-05 01:13:34 -0700

    binary tree: depth first search - preorder

--- 01_lesson.js
+++ 01_lesson.js
@@ -80,6 +80,20 @@ class BinarySearchTree {
         }
         return data
     }
+    dfs_preorder() {
+        let data = []
+
+        let current = this.root
+
+        const traverse = node => {
+            data.push(node.val)
+            if (node.left) traverse(node.left)
+            if (node.right) traverse(node.right)
+        }
+
+        traverse(current)
+        return data
+    }
 }
 
 var tree = new BinarySearchTree()
@@ -96,7 +110,7 @@ tree.insert(15)
 tree.insert(3)
 tree.insert(8)
 tree.insert(20)
-tree.bfs()
+tree.dfs_preorder()
 
 
 
