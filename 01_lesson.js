70586a57d0300aa2f8862ecefe2f8954ad81e43d
Ryan Dick <rdick@ualberta.ca>
2020-07-04 23:34:15 -0700

    binary tree: insert

--- 01_lesson.js
+++ 01_lesson.js
@@ -18,9 +18,44 @@ class BinarySearchTree {
     constructor() {
         this.root = null
     }
+    insert(val) {
+        let newNode = new Node(val)
+        console.log(val)
+        if (this.root === null) {
+            this.root = newNode
+            console.log('1')
+            return this
+        }
+        var current = this.root
+        console.log('2')
+        while (true) {
+            if (current.val > val) {
+                if (current.left === null) {
+                    current.left = newNode
+                    return this
+                } else {
+                    current = current.left
+                }
+            } else {
+                if (current.right === null) {
+                    current.right = newNode
+                    return this
+                } else {
+                    current = current.right
+                }
+            }
+        }
+    }
 }
 
 var tree = new BinarySearchTree()
-tree.root = new Node(10)
-tree.root.right = new Node(15)
-tree.root.right = new Node(7)
+// tree.root = new Node(10)
+// tree.root.right = new Node(15)
+// tree.root.left = new Node(7)
+// tree.root.left.right = new Node(9)
+
+tree.insert(10)
+tree.insert(11)
+tree.insert(12)
+
+
---
+++ check.js
@@ -0,0 +1,6 @@
+let x = 1
+
+while (x < 20) {
+    x++
+}
+console.log(x)
